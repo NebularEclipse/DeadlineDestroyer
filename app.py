787b1b578346d4115061327641faebed89b00122
Gian Carlo <nebulareclipse17@gmail.com>
2024-10-12 23:06:20 +0800

    Added several things

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/sign-up", methods=["GET", "POST"])
+def sign_up():
+    return render_template("signUp.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
